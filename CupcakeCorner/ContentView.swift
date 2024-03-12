a53952f4f97f2d1cbf4433813504de12c7ad7fed
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-03-12 12:52:44 +0500

    day 49/100 completed

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,15 +7,93 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results: [Result] = []
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disabledForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else if phase.error != nil {
+                    Text("There was an error loading the image.")
+                } else {
+                    ProgressView()
+                }
+            }
+            .frame(width: 100, height: 100)
+            
+            Form {
+                Section {
+                    TextField("Username", text: $username)
+                    TextField("Email", text: $email)
+                }
+                
+                Section {
+                    Button("Create account") {
+                        print("Creating")
+                    }
+                }
+                .disabled(disabledForm)
+            }
+            /*AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 12)
+            
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 100, height: 100)*/
+                
+            List(results, id: \.trackId) { item in
+             VStack(alignment: .leading) {
+             Text(item.trackName)
+             .font(.headline)
+             
+             Text(item.collectionName)
+             }
+             }
+             .task {
+             await self.loadData()
+             }
+             .navigationTitle("Day 49/100")
         }
-        .padding()
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=zayn+malik&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
