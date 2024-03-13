d9ccbba5940edccb54af2011151f85b168b0299f
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-03-13 10:16:57 +0500

    Day 50/100 - Adding haptic effects

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -6,28 +6,45 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
-struct Response: Codable {
+/*struct Response: Codable {
     var results: [Result]
 }
 struct Result: Codable {
     var trackId: Int
     var trackName: String
     var collectionName: String
-}
+}*/
 
 struct ContentView: View {
-    @State private var results: [Result] = []
+    /*@State private var results: [Result] = []
     @State private var username = ""
     @State private var email = ""
     
     var disabledForm: Bool {
         username.count < 5 || email.count < 5
-    }
+    }*/
+    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
         NavigationStack {
-            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            Button("Tap Count: \(counter)") {
+                counter += 1
+            }.sensoryFeedback(.increase, trigger: counter)
+            
+            Button("Tap Count: \(counter)") {
+                counter += 1
+            }.sensoryFeedback(.impact(flexibility: .soft, intensity: 0.5), trigger: counter)
+            
+            Button("Tap Count: \(counter)") {
+                counter += 1
+            }.sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
+            
+            Button("Tap Me", action: complexSuccess)
+                .onAppear(perform: prepareHaptics)
+            /*AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
                 if let image = phase.image {
                     image
                         .resizable()
@@ -75,11 +92,11 @@ struct ContentView: View {
              .task {
              await self.loadData()
              }
-             .navigationTitle("Day 49/100")
+             .navigationTitle("Day 49/100")*/
         }
     }
     
-    func loadData() async {
+    /*func loadData() async {
         guard let url = URL(string: "https://itunes.apple.com/search?term=zayn+malik&entity=song") else {
             print("Invalid URL")
             return
@@ -93,6 +110,50 @@ struct ContentView: View {
             }
         } catch {
             print("Invalid data")
+        }
+    }*/
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsAudio else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsAudio else { return }
+        
+        var events: [CHHapticEvent] = []
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        /*let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)*/
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
         }
     }
 }
