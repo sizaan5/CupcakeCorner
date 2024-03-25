143e7d0ae2a6d37a069aa9837c32ff907c8d3a7e
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-03-25 12:13:10 +0500

    day 51/100 completed - Created checkout view and order posted on internet using APIs

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -7,7 +7,20 @@
 
 import Foundation
 
-@Observable class Order {
+@Observable class Order: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Mango"]
     
     var type = 0
@@ -23,4 +36,35 @@ import Foundation
     }
     var extraFrosting = false
     var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Decimal {
+        //$2 per cake
+        var cost = Decimal(quantity * 200)
+        
+        //complicated cake
+        cost += Decimal(type/200)
+        
+        //$1/cake for extra froasting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        //$.5/cake for extra sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 200
+        }
+        return cost
+    }
 }
